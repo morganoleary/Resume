a9641eabde0924e14af49441825834b0c594973e
Morgan Oleary <morganleeoleary@gmail.com>
2024-01-16 14:07:49 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,4 @@
-// Function called when the promise resolves 
+// Function called when the promise resolves - get info from GitHub
 // user is the object being returned from the GitHub API
 // ${user.html_url} is the users public profile on GitHub
 function userInformationHTML(user) { // (@ adds the sign before the username
@@ -16,6 +16,32 @@ function userInformationHTML(user) { // (@ adds the sign before the username
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
+}
+
+// Function to display repository data on screen (rendering on the DOM)
+// (repos) is the object returned from GitHub API - returned as an array
+function repoInformationHTML(repos) {
+    // if array is empty and no repos for the user:
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`
+    }
+    // if data has been returned, iterate through array to get data out of it
+    // .map returns results of the function in an array format
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    // map returns an array so use .join to join everything w/ a new line (instead of iterating through new array again)
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("/n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -37,13 +63,18 @@ function fetchGitHubInformation(event) {
     // here we issue the promise - the .when method takes a function as 1st argument
     $.when(
         // getJSON function passes the address of github API & value of input username
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`) // list users repos
+
     ).then( // display it in the gh-user-data div
-        // response is what came back from getJSON method
-        function(response) {
-            var userData = response; // store the response in a variable
+        // response is what came back from getJSON method (2 calls = 2 responses)
+        // .when method packs response into arrays when 2 calls are there (get 1st element [0])
+        function(firstResponse, secondReponse) {
+            var userData = firstResponse[0]; // store the response in a variable
+            var repoData = secondReponse[0];
             // set the html of the div to the results of another function
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
             
             // add an error function in case the promise doesn't work out
         }, function(errorResponse) {
